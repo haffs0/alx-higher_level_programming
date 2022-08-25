5d09f45b48b7f5430d27f23d86a359e9470e2139
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-08-25 11:40:07 +0000

    print command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     if argv_len == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(argv_len - 1))
+        if argv_len == 2:
+            print("{} argument:".format(argv_len - 1))
+        else:
+            print("{} arguments:".format(argv_len - 1))
 
         for index in range(1, argv_len):
             print("{}: {}".format(index, argvs[index]))
