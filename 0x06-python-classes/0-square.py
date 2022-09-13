c06005072efbc7954a82e9c976535b92e303acb2
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-09-13 16:31:56 +0000

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""An empty square class struct"""
+
+
 class Square:
-
     """create a square class."""
-
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""A square class with private attribute instance"""
+
+
 class Square:
     """creates a square class object."""
 
