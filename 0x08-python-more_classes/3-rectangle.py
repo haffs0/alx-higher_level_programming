8808e30366f492592edc7f06a524a363a7b2db20
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-09-21 17:02:44 +0000

    print rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+THis 2-rectangle module has one class
+"""
+
+
+class Rectangle:
+    """initialized data"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """change width value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        rectangle = []
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            rectangle.append("#" * self.__width)
+            if i != self.__height - 1:
+                rectangle.append("\n")
+        return ("".join(rectangle))
