de0a328f5448677e4fcdfd6fe0bf83bb3ac98947
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-09-27 20:49:46 +0000

    add files

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ import json
 def load_from_json_file(filename):
     """load from Json file"""
     with open(filename, mode='r', encoding="utf-8") as f:
-       return json.load(f)
+        return json.load(f)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This 8-class_to_json module has one function, class_to_json(obj)
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """class to json"""
+    obj.__dict__
