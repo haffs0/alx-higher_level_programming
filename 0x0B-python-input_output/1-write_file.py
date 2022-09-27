1248faf5837bd51467ae00f71fac9b87037e0ee2
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-09-27 17:43:45 +0000

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ This 1-write_file module has one function, write_file(filename="", text="")
 
 def write_file(filename="", text=""):
     """Read a file"""
-    with open(filename, 'r+') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
