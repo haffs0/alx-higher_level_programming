bd1283583f6a9492c4a874975f60be4c1768f049
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-11-02 17:14:28 +0000

    advanced

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -8,5 +8,13 @@ SELECT DISTINCT `name`
 
        INNER JOIN `tv_shows` AS t
        ON s.`show_id` = t.`id`
-       WHERE t.`title` <> "Dexter"
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
+
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
  ORDER BY g.`name`;
