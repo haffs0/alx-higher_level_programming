548e8e902262d740d984f68962d7999f82b7e412
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-01-17 14:36:03 +0000

    add scripts

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-	if (error) console.log(error);
-	else console.log(JSON.parse(body).title);
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
