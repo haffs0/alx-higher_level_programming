ff38ec7f149088fed9553529172e8a974f00b781
haffs0 <abuafeezabolarinwa@yahoo.com>
2023-01-17 14:44:04 +0000

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      let task = json[i];
+      if (task.completed) {
+        if (results[task.userId] === undefined) {
+          results[task.userId] = 1;
+        } else {
+          results[task.userId] += 1;
+        }
+      }
+    }
+    console.log(results);
+  }
+});
